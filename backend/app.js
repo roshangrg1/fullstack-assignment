f6eefe40b4ad3ade9cfc0f52b05a5e5a31326bf0
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 15:23:15 +0545

    Setup : for files upload (image of Books)

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,19 @@ require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+// file upload
+const cloudinary = require("cloudinary");
+const fileUpload = require('express-fileupload');
 
 const app = express()
+
+// cloudinary cofiguration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 
 // middlewares
 app.use(express.json())
@@ -14,6 +25,12 @@ app.use(cookieParser())
 
 // Morgan logger
 app.use(morgan('tiny'))
+
+// Note that this option available for versions 1.0.0 and newer. 
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : '/tmp/'
+}));
 
 // importing routes
 const user = require("./routes/user")
