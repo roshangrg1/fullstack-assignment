25eec5387ddc5da2c0891e107643d81c2821b1a0
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 13:51:33 +0545

    Routes Added: profile

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { signup ,login, logout ,getProfile} = require("../controllers/auth.controller");
-
+const {isLoggedIn} = require('../middlewares/auth.middleware')
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").get(logout)
-
+router.route("/profile").get(isLoggedIn, getProfile)
 module.exports = router;
