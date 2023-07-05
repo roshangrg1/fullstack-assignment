37ee777e3fe32720d3ce0ba3dd90d80f87f430b5
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 16:42:31 +0545

    Routes Added: getall books

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 
-const { addBook} = require("../controllers/book.controller");
+const { addBook, adminGetAllBooks} = require("../controllers/book.controller");
 const {isLoggedIn, customRole} = require('../middlewares/auth.middleware')
 
 // Admin routes
 router
   .route("/admin/book/add")
   .post(isLoggedIn, customRole("admin"), addBook);
+
+  router.get('/admin/books', isLoggedIn,customRole('admin'),adminGetAllBooks)
+
 
 
 module.exports = router;
