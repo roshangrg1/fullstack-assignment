7abf34d7705a13d48984e2d1b23074f0b35437df
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 17:36:38 +0545

    Routes Added: delete books

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const { addBook, adminGetAllBooks,adminUpdateOneBook} = require("../controllers/book.controller");
+const { addBook, adminGetAllBooks,adminUpdateOneBook,adminDeleteOneBook} = require("../controllers/book.controller");
 const {isLoggedIn, customRole} = require('../middlewares/auth.middleware')
 
 // Admin routes
@@ -15,6 +15,7 @@ router
   router
   .route("/admin/book/:id")
   .put(isLoggedIn, customRole("admin"), adminUpdateOneBook)
+  .delete(isLoggedIn, customRole("admin"),adminDeleteOneBook );
 
 
 
