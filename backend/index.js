9d98a50caa41c47963c2bb59aa7857fef1662cb3
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 12:05:52 +0545

    Connection: Database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const app = require("./app");
+const connectWithDb = require('./config/database')
 
+
+connectWithDb()
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is running at port: ${process.env.PORT}`);
