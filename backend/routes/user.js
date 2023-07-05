22eafb8b3dea583d08efc22618f9ca04dd4f22d1
Roshan Guragain <twistfreaky@gmail.com>
2023-07-05 14:13:22 +0545

    Routes Added:  admin/users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup ,login, logout ,getProfile} = require("../controllers/auth.controller");
-const {isLoggedIn} = require('../middlewares/auth.middleware')
+const { signup ,login, logout ,getProfile,adminAllUser} = require("../controllers/auth.controller");
+const {isLoggedIn, customRole} = require('../middlewares/auth.middleware')
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").get(logout)
 router.route("/profile").get(isLoggedIn, getProfile)
+
+// Admin only routes
+router.route("/admin/users").get(isLoggedIn, customRole("admin"), adminAllUser);
 module.exports = router;
