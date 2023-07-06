95181dc6122525946c483762ea491af52f863bc8
Roshan Guragain <twistfreaky@gmail.com>
2023-07-06 10:04:41 +0545

    Controller Added: create order

---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,31 @@
+const Order = require("../models/order.schema");
+const tryCatchHandler = require("../utils/tryCatchHandler");
+const CustomError = require("../utils/customError");
+
+
+exports.createOrder = tryCatchHandler(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    user: req.user._id,
+  });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
