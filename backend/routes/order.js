8bf41a13a63d9fcf5e1ccb9bceaf1abf52e264b5
Roshan Guragain <twistfreaky@gmail.com>
2023-07-06 13:10:21 +0545

    Routes Added: admingetAllOrders

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,15 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {
   createOrder,
-  getOneOrder
+  getOneOrder,
+  getLoggedInOrders,
+  admingetAllOrders
 } = require("../controllers/order.controller");
 
-const { isLoggedIn, } = require("../middlewares/auth.middleware");
+const { isLoggedIn, customRole } = require("../middlewares/auth.middleware");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
 router.route("/order/:id").get(isLoggedIn, getOneOrder);
+router.route("/myorder").get(isLoggedIn, getLoggedInOrders);
 
-
+router
+  .route("/admin/orders")
+  .get(isLoggedIn, customRole("admin"), admingetAllOrders);
 
 
 module.exports = router;
