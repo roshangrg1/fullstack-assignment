91ba898c9f7cec600e87a74913503484daa0874d
Roshan Guragain <twistfreaky@gmail.com>
2023-07-06 18:33:57 +0545

    Component added: footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./component/layout/Header/Header";
 import { BrowserRouter, Route } from "react-router-dom";
+import Footer from "./component/layout/footer/Footer";
 function App() {
   return (
     <BrowserRouter>
       <Header />
+
+      <Footer/>
     </BrowserRouter>
   );
 }
