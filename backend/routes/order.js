1b23e48679599cfbb18004fe200a493b7f446ca9
Roshan Guragain <twistfreaky@gmail.com>
2023-07-06 10:13:30 +0545

    Routes Added: getOneOrder

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   createOrder,
-
+  getOneOrder
 } = require("../controllers/order.controller");
 
 const { isLoggedIn, } = require("../middlewares/auth.middleware");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
+router.route("/order/:id").get(isLoggedIn, getOneOrder);
 
 
 
