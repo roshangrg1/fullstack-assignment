3c24aa8c5b4f52e257b6f2993d8107e6c4d603db
Roshan Guragain <twistfreaky@gmail.com>
2023-07-06 10:03:52 +0545

    order routes setup

--- backend/app.js
+++ backend/app.js
@@ -35,11 +35,12 @@ app.use(fileUpload({
 // importing routes
 const user = require("./routes/user")
 const book = require('./routes/book')
+const order = require('./routes/order')
 
 // router middlewares
 app.use("/api/v1", user);
 app.use('/api/v1', book)
-
+app.use('/api/v1', order)
 app.get("/", (req, res) => {
     res.send("home");
   });
